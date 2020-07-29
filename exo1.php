9ae181550d669efde44d50eddab006f34440c7b9
hellycia <wattiaux.helene@hotmail.fr>
2020-07-29 08:58:04 +0200

    Remove condition

--- exo1.php
+++ exo1.php
@@ -6,8 +6,5 @@ while($nombre <1 || $nombre > 3){
     echo("Vous devez rentrer un nombre entre 1 et 3 \n");
     $nombre = readline("Donnez un nombre entre 1 et 3: ");
 }
-
-if($nombre >= 1 || $nombre <= 3){
-    exit();
-}
+exit();
 ?>
